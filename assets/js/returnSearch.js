c74897902d66bec197a0d6a3f04bb56f1e1a5304
hamzsait <hamzasait@utexas.edu>
2021-04-07 20:41:11 -0500

    resolved local storage bug

--- assets/js/returnSearch.js
+++ assets/js/returnSearch.js
@@ -39,7 +39,7 @@ var requestOptions = {
 function search (){
 
   requestURL = String("https://api.themoviedb.org/3/search/movie?api_key=230e89ce98b6d55971d6dd92298b9018&query=" + document.getElementById("search").value)
-  console.log(requestURL)
+
   fetch(requestURL, requestOptions)
     .then(response => response.text())
     .then(result => displayText(JSON.parse(result)))
@@ -48,15 +48,20 @@ function search (){
 
 
 function displayText(file){
-  console.log(file)
+
   parent = document.querySelector("#results")
 
   parent.innerHTML = ""
 
-  for (x = 0; x < 10; x++){
+  for (x = 0; x < file.results.length; x++){
+
+
+    var title = document.createElement('li')
+    title.textContent = file.results[x].title
+    title.setAttribute("class","listItem")
+    title.style.padding = "3px"
     favorite = document.createElement("button")
     contain = document.createElement("div")
-    title = document.createElement('li')
 
     contain.style.display = "flex"
 
@@ -66,10 +71,7 @@ function displayText(file){
     star.setAttribute("id","star")
     favorite.append(star)
     contain.appendChild(favorite)
-
-    title.textContent = file.results[x].title
-    title.setAttribute("class","listItem")
-    title.style.padding = "3px"
+    
     contain.appendChild(title)
 
     document.querySelector("#results").appendChild(contain)
@@ -94,40 +96,54 @@ $(document).on('click', '.closemodal', function(){
   movieModal.classList.toggle("is-active");
 })
 
+function uniq(a) {
+  return Array.from(new Set(a));
+}
+
 function favorites(){
   $(".favorite").on("click",function(){
     if ($(this).css("background-color") == "rgb(239, 239, 239)"){
       $(this).css("background-color", "yellow")
-      updateLocalStorage(this)
+      updateLocalStorage(this, true)
     }
     else{
       $(this).css("background-color", "rgb(239, 239, 239)")
-        updateLocalStorage(this)
+        updateLocalStorage(this, false)
       }
     })
   }
 
-function updateLocalStorage(start){
 
-  favoriteMovies = []
+function updateLocalStorage(start, selected){
 
-  var items = ($(start).parent().parent().children())
-
-  console.log(items)
-
-  for (x = 0; x<items.length; x++){
-    if (($($($(items[x]).children()[0])).css("background-color") == "rgb(255, 255, 0)")){
-
-      console.log(($($($(items[x]).children()[0])).css("background-color") == "rgb(255, 255, 0)"))
-
-      adder = ($($(items[x]).children()[1])[0].outerText)
-      
-      favoriteMovies.push(adder)
+    if(selected){
+      if (localStorage.length > 0){
+        console.log("if")
+        var movies = []
+        movies.push(($(start).parent().children()[1].outerText))
+        movies.push((localStorage.getItem("favorites")).split(','))
+        console.log(movies)
+        localStorage.setItem("favorites",movies)
+      }
+      else{
+        console.log("else")
+        var movies = []
+        movies.push($(start).parent().children()[1].outerText)
+        localStorage.setItem("favorites",movies)
+      }
     }
-  }
+    else{
+      target = ($(start).parent().children()[1].outerText)
+      favoriteList = (localStorage.getItem("favorites").split(","))
 
-  console.log(favoriteMovies)
-    
+      for (x = 0 ; x<favoriteList.length; x++){
+        if (favoriteList[x] == target){
+          favoriteList.splice(x,1)
+        }
+      }
+      localStorage.setItem("favorites",favoriteList)
+    }
+  
 }
 
 
